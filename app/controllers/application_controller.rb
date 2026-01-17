fe32ef2ba588565594a974050a8bba0ede548c6b
Akibuddin2pronto <mohd.akibuddin@prontonetworks.ai>
2026-01-17 21:40:42 +0530

    ETA06 updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,11 @@ class ApplicationController < ActionController::Base
 
   # Devise: Permit additional parameters for sign up and account update
   before_action :configure_permitted_parameters, if: :devise_controller?
-end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
+end
